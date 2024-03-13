a6a16eb7afa799115d9145f918ff20847cbe5946
BajaLiau <liauweisheng@hotmail.com>
2024-03-13 22:27:53 +0800

    assignment folder

---
+++ assignment/header.php
@@ -0,0 +1,30 @@
+<html>
+
+<!DOCTYPE html>
+
+<head>
+<style>
+#logo {height: 50px; width: 50px;}
+#user {height:50px;width:50px;position: absolute;right:20px;}
+#cart {height:50px;width:50px;position: absolute;right:80px;}	
+</style>
+</head>
+
+<body>
+<header>
+	<nav>
+		<a href="/assignment">
+			<img id = "logo" src="image/logo.png">
+		</a>
+
+		<a>
+			<img id = "user" src="image/user.png">
+		</a>
+		
+		<a>
+			<img id = "cart" src="image/cart.png">
+		</a>
+	</nav>
+</header>
+
+</body>
