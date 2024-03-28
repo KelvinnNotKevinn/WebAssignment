bd9cbe29c6be8fe218543dc27743ef433ed12c2f
夜长梦多 <limyoule09@gmail.com>
2024-03-29 03:52:09 +0800

    changing

--- Login/login.js
+++ Login/login.js
@@ -4,3 +4,16 @@ const SignUpButton = document.querySelector('#SignUp');
 
 SignUpButton.addEventListener('click',() => container.classList.add('right-panel-active'))
 LoginButton.addEventListener('click',() => container.classList.remove('right-panel-active'))
+
+function validatePasswordIsMatch() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword)
+    {
+        alert("Passwords do not match");
+        return false;
+    }
+
+    return true;
+}
