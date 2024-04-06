166b9ca0e4b35b890497561622747188eab84e70
KelvinnNotKevinn <kwakhaimun@gmail.com>
2024-04-06 18:29:27 +0800

    arrabnge

--- Login/CreateUser/index.php
+++ Login/CreateUser/index.php
@@ -14,7 +14,10 @@
 </head>
 
 <body>
+<!-- create a section for create user -->
 	<section id="createUserPage">
+	
+		<!-- a form to input user info -->
 		<form method="post" id="userSignUp" action="createUser.php">
 			<label for='username'>Username</label>
 			<input id='username' name='username' type = "text">
@@ -36,7 +39,46 @@
 		</form>
 	</section>
 	
-	<script src="login.js"> </script>
+	<?php
+
+	$name = $_POST["username"];
+	$password = $_POST["password"];
+	$email = $_POST["email"];
+
+	$servername = "localhost";
+	$dbusername = "Admin"; //root
+	$dbpassword = "Sun030904"; //
+	$dbname = "user";
+
+	$conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
+
+	if(!$conn)
+	{
+		die("Connection failed: ".mysqli_connect_error());
+	}
+
+	$newUser = "INSERT INTO user (username,password,email) VALUES(?,?,?)";
+
+	$input = mysqli_prepare($conn,$newUser);
+
+	mysqli_stmt_bind_param($input, "sss", $name, $password, $email);
+
+	if(mysqli_stmt_execute($input))
+	{
+		echo "create user successfully!";
+	}
+	else
+	{
+		echo"Error inserting record".mysqli_error($conn);
+	}
+
+	mysqli_stmt_close($input);
+	mysqli_close($conn);
+
+	header("Location: index.php");
+	exit;
+
+	?>
 </body>
 
 </html>
