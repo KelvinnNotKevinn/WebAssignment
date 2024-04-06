fd252de3ae290d3b8f0b0790d57e7a20ecd1cf05
夜长梦多 <limyoule09@gmail.com>
2024-04-06 12:36:32 +0800

    update

--- account/createUser.php
+++ account/createUser.php
@@ -1,46 +1,4 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
-	$userName = $_POST["newUserName"];
-	$email = $_POST["email"];
-	$password = $_POST["newPassword"];
-	$confirmPassword = $_POST['confirmPassword'];
-
-	$errors = [];
-
-	if (empty($newUserName))
-    {
-        $errors['newUserName'] = '*Please create a Username for your account';
-    }
-
-    if (empty($email))
-    {
-        $errors['email'] = '*Please enter your email';
-    }
-    else if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-    {
-        $errors['email'] = 'Please enter a valid email';
-    }
-
-    if (empty($newPassword))
-    {
-        $errors['newPassword'] = '*Please create a password for your account';
-    }
-
-    if (empty($confirmPassword))
-    {
-        $errors['confirmPassword'] = 'Please enter the password again';
-    }
-    else if ($confirmPassword != $newPassword)
-    {
-        $errors['confirmPassword'] = 'Passwords do not match';
-    }
-
-	if (!empty($errors))
-	{
-		include ('index.php');
-		exit;
-	}
 
     $servername = "localhost";
 	$dbusername = "root";
@@ -58,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
 	$input = mysqli_prepare($conn,$newUser);
 
-	mysqli_stmt_bind_param($input, "sss", $userName, $email, $password);
+	mysqli_stmt_bind_param($input, "sss", $newUserName, $email, $newPassword);
 
 	if(mysqli_stmt_execute($input))
 	{
@@ -74,5 +32,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
 	header("Location: index.php");
 	exit;
-}
+
 ?>
