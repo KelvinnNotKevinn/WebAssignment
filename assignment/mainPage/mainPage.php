d09e58bfbeba01281a715e5cde83f4bff4103dee
BajaLiau <liauweisheng@hotmail.com>
2024-04-06 12:15:38 +0800

    mainpage

---
+++ assignment/mainPage/mainPage.php
@@ -0,0 +1,37 @@
+<html>
+<!DOCTYPE html>
+
+<head>
+	<title>Mainpage</title>
+	<link rel="stylesheet" href="mainPage.css"> 
+</head>
+	
+<body>
+	<header></header>
+	
+	<section id="firstSection">
+		<p id="companyName">KNOW YOUR BEST BUILD<p>
+		<p></p>
+	</section>
+	
+	<section id="secondSection">
+		<img src="" id="section2Logo">
+		<a href="#" id="section2Link"> About Us </a>
+	</section>
+	
+	<section id="thirdSection">
+		<h1>Hot Sale Product<h1>
+		<a href="#"> see more </a>
+	</section>
+	
+	<footer></footer>
+	
+</body>
+
+
+
+
+
+
+
+</html>
