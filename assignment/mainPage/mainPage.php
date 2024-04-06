c05ab8f6062e5c7620c88d0dcd43556e115d7595
BajaLiau <liauweisheng@hotmail.com>
2024-04-06 22:17:21 +0800

    mainpage

--- assignment/mainPage/mainPage.php
+++ assignment/mainPage/mainPage.php
@@ -4,24 +4,38 @@
 <head>
 	<title>Mainpage</title>
 	<link rel="stylesheet" href="mainPage.css"> 
+	<script src="translate.js"></script>
 </head>
 	
 <body>
 	<header></header>
 	
 	<section id="firstSection">
+	<div>
 		<p id="companyName">KNOW YOUR BEST BUILD<p>
-		<p></p>
+		<img src="/assignment/image/logo-white.png" id="section2Logo">
+		<!-- <a href="#" id="section2Link"> About Us </a> -->
+	</div>
 	</section>
 	
-	<section id="secondSection">
-		<img src="" id="section2Logo">
-		<a href="#" id="section2Link"> About Us </a>
-	</section>
+	<section>
+	<!-- Slideshow container -->
+	<div class="customizePCContainer">
+
+		<!-- Full-width images with number and caption text -->
+		<div class="showing customizePC fade">
+			<img src="/assignment/image/logo-white.png" style="width:100%">
+		</div>
+		
+		<div class="customizePC fade">
+			<img src="/assignment/image/logo.png" style="width:100%">
+		</div>
+		 
+		<!-- Next and previous buttons -->
+		<a class="prev" onclick="plusPC(-1)">&#10094;</a>
+		<a class="next" onclick="plusPC(1)">&#10095;</a>
 	
-	<section id="thirdSection">
-		<h1>Hot Sale Product<h1>
-		<a href="#"> see more </a>
+	</div>
 	</section>
 	
 	<footer></footer>
