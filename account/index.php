cc15ee4c6085aeceddf8f4432fab2b5ee57749f6
夜长梦多 <limyoule09@gmail.com>
2024-04-11 18:42:08 +0800

    modify

--- account/index.php
+++ account/index.php
@@ -21,6 +21,7 @@ if (isset($_SESSION['userName']))//check user login or not
 <html lang = "en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KKYB PC Build</title>
     <link rel = "stylesheet" href = "index.css">
