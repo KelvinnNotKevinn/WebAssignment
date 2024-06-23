016e3197e2809b5871da79cede53e4e39af84d7a
BajaLiau <liauweisheng@hotmail.com>
2024-06-23 23:54:48 +0800

    done

---
+++ Buildpc/Insert Product data.sql
@@ -0,0 +1,32 @@
+INSERT INTO PCcase(id, name, `desc`, img) VALUES ('P001','DAYLIGHT','High-performance, sleek white build that combines power and style for any computing need','white.jpg');
+INSERT INTO PCcase(id, name, `desc`, img) VALUES ('P002','ROSELIA',"Stylish pink build that doesn't compromise on performance, perfect for those who want a standout look without sacrificing power",'pink.jpg');
+INSERT INTO PCcase(id, name, `desc`, img) VALUES ('P003','NIGHTWING',"Formidable black build, marrying sleek design with high-performance PCcase, ready to conquer any task or gaming challenge with style and power",'black.jpg');
+INSERT INTO PCcase(id, name, `desc`, img) VALUES ('P004','AQUA',"Experience the power of the Aqua PC, blending sleek blue aesthetics with tailored performance for gaming, creativity, and multitasking.",'blue.jpg');
+
+
+INSERT INTO components(id, name, price, category) VALUES ('C001','AMD B450M GAMING MOTHERBOARD',425,'MOTHERBOARD');
+INSERT INTO components(id, name, price, category) VALUES ('C002','ASUS STRIX Z270F GAMING MOTHERBOARD',483,'MOTHERBOARD');
+INSERT INTO components(id, name, price, category) VALUES ('C003','ASUS TUF B550M-PLUS GAMING MOTHERBAORD',628,'MOTHERBOARD');
+INSERT INTO components(id, name, price, category) VALUES ('C004','AMD RYZEN 5 5500',399,'PROCESSOR');
+INSERT INTO components(id, name, price, category) VALUES ('C005','Intel® Core™ i5-12400F',619,'PROCESSOR');
+INSERT INTO components(id, name, price, category) VALUES ('C006','Intel® Core™ i7-12700F',1199,'PROCESSOR');
+INSERT INTO components(id, name, price, category) VALUES ('C007','ADATA XPG SPECTRIX 8GB DDR4 3200MHZ',115,'RAM');
+INSERT INTO components(id, name, price, category) VALUES ('C008','KINGSTON HYPERX FURY 8GB DDR4 3200MHZD',162,'RAM');
+INSERT INTO components(id, name, price, category) VALUES ('C009','ADATA XPG SPECTRIX 16GB DDR4 3600MHZ',235,'RAM');
+INSERT INTO components(id, name, price, category) VALUES ('C010','GIGABYTE GTX 1650 WINDFORCE 4GB',1159,'GPU');
+INSERT INTO components(id, name, price, category) VALUES ('C011','ZOTAC RTX 4060 TWIN EDGE 8GB',1520,'GPU');
+INSERT INTO components(id, name, price, category) VALUES ('C012','ASUS RTX 4070 DUAL GEFORCE 12GB',3892,'GPU');
+INSERT INTO components(id, name, price, category) VALUES ('C013','WD SA510 500GB SATA SSD',308,'STORAGE');
+INSERT INTO components(id, name, price, category) VALUES ('C014','XPG S60 1TB PCIE SSD',416,'STORAGE');
+INSERT INTO components(id, name, price, category) VALUES ('C015','SAMSUNG 980 1TB NVME M.2 SSD',642,'STORAGE');
+
+INSERT INTO products (id, name, `desc`, spec, img, price) VALUES
+('D001','G203 LIGHTSYNC', 'Make the most of play time with G203—a gaming mouse in a variety of vibrant colors.','LIGHTSYNC RGB lighting 6 programmable buttons Resolution: 200 – 8,000 dpi', 'g203.png', 73.00),
+('D002','G305 LIGHTSPEED', 'LIGHTSPEED wireless gaming mouse designed for serious performance with latest technology innovations.','Wireless report rate: 1000Hz Wireless technology: LIGHTSPEED Wireless Microprocessor: 32-bit ARM', 'g305ls.png', 139.00),
+('D003','G502 HERO', 'Engineered for advanced gaming performance. G502 HERO features HERO 25K gaming sensor.','Sensor: HERO Resolution: 100 – 25,600 dpi Zero smoothing/acceleration/filtering Max. acceleration: >40 G 1Tested on Logitech G240 Gaming Mouse PadMax. speed: >400 IPS 2 Tested on Logitech G240 Gaming Mouse Pad', 'g502hr.png', 169.00),
+('D004','G413 SE', 'From tactile mechanical switches to 6-key rollover anti-ghosting and PBT keycap to compete.','Actuation distance: 1.9 mm Actuation force: 50 g Total travel distance: 4 mm Connection Type: USB 2.0 USB Protocol: USB 2.0 Backlighting: Yes, white per key lighting', 'g413.png', 271.00),
+('D005','G213 PRODIGY', 'Durable with gaming-grade performance. Tactile Mech-Dome keyswitches are spill-resistant.','Connection Type: USB 2.0 USB Protocol: USB 2.0 USB Speed: Full Speed Indicator Lights: Yes Backlighting: RGB Cable Length : 1.8 m', 'g213.png', 179.00),
+('D006','PRO X TKL', 'A championship-trusted wireless gaming keyboard designed for the highest levels of competitive play.','Pro-inspired tenkeyless design LIGHTSYNC RGB lighting. Onboard lighting profiles . 6 ft detachable charging and data cable1 ms report rate', 'prox.png', 799.00),
+('D007','G335 Gaming Headset', 'A lightweight, cool wired headset made with a suspension headband design with an adjustable strap.','Driver: 40 mm Frequency Response: 20 Hz - 20 kHz Impedance: 36 Ohms Sensitivity: 87.5 dB SPL/mW', 'g335.png', 260.00),
+('D008','G733 Wireless Headset', 'Wireless gaming headset designed for performance and comfort. Outfitted with all the surround sound.','Driver: PRO-G 40 mm Frequency Response: 20 Hz - 20 kHz Impedance: 39 Ohms , 5k Ohms  Sensitivity: 87.5 dB SPL/mW', 'g733.png', 600.00),
+('D009','PRO X 2 LIGHTSPEED', 'Designed with pros for the highest levels of competition. Wireless and built with the highest quality material.','Driver: Graphene 50 mm Magnet: Neodymium Frequency Response: 20 Hz-20 KHz Impedance: 38 Ohms Sensitivity: 87.8 dB SPL @ 1 mW & 1 cm', 'prox2.png', 1214.00);
